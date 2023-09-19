a158039a05032663b730db7603f7c4d96e42d25f
Sehrish Noor <122004828+Angel-noori@users.noreply.github.com>
2023-09-19 18:49:25 +0500

    www

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -14,7 +14,7 @@ class Var(object):
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
     BIN_CHANNEL = int(-1001696981337)
-    PORT = int(getenv('PORT', 5000))
+    PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     OWNER_ID = int(getenv('OWNER_ID', '1875452714'))
     NO_PORT = bool(getenv('NO_PORT', False))
@@ -25,7 +25,7 @@ class Var(object):
         APP_NAME = str(getenv('APP_NAME'))
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('hbmoviestream-9662c5fd6d9b.herokuapp.com') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
     DATABASE_URL = str("mongodb+srv://123:123@cluster0.f5pm3sh.mongodb.net/?retryWrites=true&w=majority")
