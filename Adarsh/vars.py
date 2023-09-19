0245cf30bf64b98995be757d6f8349a75ce0295f
Jaspreet Singh <123469919+Jass6268@users.noreply.github.com>
2023-09-19 21:55:07 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -14,7 +14,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001696981337'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001880852863'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -36,4 +36,4 @@ class Var(object):
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://123:123@cluster0.f5pm3sh.mongodb.net/'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', '-1001655442572'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001696981337")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001880852863")).split()))
