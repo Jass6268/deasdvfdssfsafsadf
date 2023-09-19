858f12f9df2546a1f0aa4320559afeca57f3d8c3
Jaspreet Singh <123469919+Jass6268@users.noreply.github.com>
2023-09-19 16:13:47 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 class Var(object):
-    API_ID = int(15037283)
-    API_HASH = str("7af9d761267bf6b81ed07f942d87127f")
-    BOT_TOKEN = str("6685706580:AAHkEePNy21IsAJ0sTLE_2EKh0D9qNU_0WY")
-    SESSION_NAME = str(getenv('SESSION_NAME', 'MCFILESTREAMER'))
+    API_ID = int(5390465)
+    API_HASH = str("abafb7520db145a9157b09c066f9a399")
+    BOT_TOKEN = str("6049118328:AAFNM4tTdktPFI98tIUt9anCVSDTIxbnGBI")
+    SESSION_NAME = str(getenv('SESSION_NAME', 'HBFILESTREAMER'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(-1001645923712)
+    BIN_CHANNEL = int(-1001880852863)
     PORT = int(getenv('PORT', 5000))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'marvelcloud.starkai.live'))
-    OWNER_ID = int(getenv('OWNER_ID', '2043144248'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', 'livestreamfun.xyz'))
+    OWNER_ID = int(getenv('OWNER_ID', '1875452714'))
     NO_PORT = bool(getenv('NO_PORT', True))
     APP_NAME = None
-    OWNER_USERNAME = str("MarvelCloud")
+    OWNER_USERNAME = str("HBMoviesGod")
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -28,6 +28,6 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
         "http://{}:{}/".format(FQDN, PORT)
-    DATABASE_URL = str("mongodb+srv://stark:stark@cluster0.9sxyrqb.mongodb.net/")
+    DATABASE_URL = str("mongodb+srv://123:123@cluster0.f5pm3sh.mongodb.net/")
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001696981337")).split()))
