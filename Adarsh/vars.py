1ed6202ed168e847efe604b05603e31873678d77
Jaspreet Singh <123469919+Jass6268@users.noreply.github.com>
2024-01-17 18:42:01 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -8,20 +8,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', '5390465'))
-    API_HASH = str(getenv('API_HASH', 'abafb7520db145a9157b09c066f9a399'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', '6049118328:AAFNM4tTdktPFI98tIUt9anCVSDTIxbnGBI'))
+    API_ID = int(getenv('API_ID', ''))
+    API_HASH = str(getenv('API_HASH', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001880852863'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1875452714").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'hbmoviesgod'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', ''))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -34,6 +34,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://123:123@cluster0.f5pm3sh.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'hbmoviesfun'))
+    DATABASE_URL = str(getenv('DATABASE_URL', ''))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', ''))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
